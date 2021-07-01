7c5d1968f170c628d6c29caab482697c16aa7962
ola <perfectfind94@gmail.com>
2021-07-01 10:26:25 +0100

    Added firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Firebase and Firestore
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: "AIzaSyAqXskhRHylMf86lFJ33omYh8-wcm3TB5c",
+  authDomain: "cardmiral.firebaseapp.com",
+  projectId: "cardmiral",
+  storageBucket: "cardmiral.appspot.com",
+  messagingSenderId: "297380688108",
+  appId: "1:297380688108:web:2f9364e2e6afbac3a975c9"
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +30,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
