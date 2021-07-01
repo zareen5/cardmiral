09363e0712876faa68b663ed7493e72d92f32fe3
Safura <zareensafura25@gmail.com>
2021-07-01 10:58:21 +0100

    Added new-card and add wishes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddWishesComponent } from './add-wishes/add-wishes.component';
+import { CreateCardComponent } from './create-card/create-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'new-card', component: CreateCardComponent },
+ { path: 'add-wishes', component: AddWishesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CreateCardComponent } from './create-card/create-card.component';
+import { AddWishesComponent } from './add-wishes/add-wishes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCardComponent,
+    AddWishesComponent
   ],
   imports: [
     BrowserModule,
